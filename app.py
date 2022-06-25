d242c81cfb4059360badb00128ebd186033e35bd
khadra <67019030+khadra1@users.noreply.github.com>
2022-06-25 10:38:50 +0100

    connect db

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from scripts.main import load_data
 
 app = Flask(__name__)
-db_url = "postgres://njuguscorkabpk:14f0dd2bc05cce718487cbff320b81dd7088ba958ac59cc07c82152d4b23909f@ec2-34-225-159-178.compute-1.amazonaws.com:5432/d6sa1vahv1mif0"
+db_url = "postgresql://njuguscorkabpk:14f0dd2bc05cce718487cbff320b81dd7088ba958ac59cc07c82152d4b23909f@ec2-34-225-159-178.compute-1.amazonaws.com:5432/d6sa1vahv1mif0"
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 db = SQLAlchemy(app)
 
