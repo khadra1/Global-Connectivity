999b49b4513fa5bc6b0c473051b25c563f1655f0
khadra <67019030+khadra1@users.noreply.github.com>
2022-06-25 10:31:12 +0100

    connect db

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import countries_data
+from countries_data import clean_data
 def load_data():
-    clean_world_data = countries_data.clean_data()
+    clean_world_data = clean_data()
     return clean_world_data
 
 
-print (load_data())
+# print (load_data())
 
 
