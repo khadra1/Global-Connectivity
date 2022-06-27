f43726bbce583f6099d807f47aa2b01b215cabf8
khadra <67019030+khadra1@users.noreply.github.com>
2022-06-27 16:27:05 +0100

    data added

--- scripts/countries_data.py
+++ scripts/countries_data.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
 def clean_data():
-    world_data = pd.read_csv("../data/world_bank_internet_data.csv", skiprows=[2395,2396,2397,2398,2399,2400])
-    world_data.replace("..",0)
-
-    world_data.rename(columns={"Country Name": "Country"}, inplace=True)
-
-    coordinates = pd.read_csv("../data/world_coordinates.csv")
-
-    final_world = pd.merge(world_data, coordinates, on="Country")
+    # Import the world internet data from world bank and rename columns for merging with location data
+    world_data= pd.read_csv("../data/world_bank_internet_data.csv", skiprows=[2395,2396,2397,2398,2399,2400])
+    world_data = world_data.replace("..",0)
+    world_data.rename(columns={"Country Name": "Country", "Country Code": "Codes"}, inplace=True)
+    #Import the coordinate csv file, rename columns for merging with each other later on
+    coordinates = pd.read_csv("..data/world_country.csv")
+    coordinates.rename(columns={"iso_con" : "Codes", "lat": "Latitude", "lon": "Longitude"},inplace=True)
+    coordinates.drop(columns=["Unnamed: 0", "country"],inplace=True)
+    # Merge the world_data with the coordinates
+    final_world = pd.merge(world_data, coordinates, on="Codes", how="left")
+    
     return final_world
--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import os 
-# from countries_data import clean_data
+
 def load_data():
+    # Import the world internet data from world bank and rename columns for merging with location data
     data_path = os.path.join(os.path.dirname(__file__), '..', 'data', "world_bank_internet_data.csv")
-      
     world_data = pd.read_csv(data_path, skiprows=[2395,2396,2397,2398,2399,2400])
     world_data.replace("..",0)
-    world_data.rename(columns={"Country Name": "Country"}, inplace=True)
-
-    data_path = os.path.join(os.path.dirname(__file__), '..', 'data', "world_coordinates.csv")
+    world_data.rename(columns={"Country Name": "Country", "Country Code": "Codes"}, inplace=True)
+    # Import the coordinate csv file, rename columns for merging with each other later on
+    data_path = os.path.join(os.path.dirname(__file__), '..', 'data', "world_country.csv")
     coordinates = pd.read_csv(data_path)
-
-    final_world = pd.merge(world_data, coordinates, on="Country")
-    print(final_world["2014 [YR2014]"])
+    coordinates.rename(columns={"iso_con" : "Codes", "lat": "Latitude", "lon": "Longitude"},inplace=True)
+    coordinates.drop(columns=["Unnamed: 0", "country"],inplace=True)
+    # Merge the world_data with the coordinates
+    final_world = pd.merge(world_data, coordinates, on="Codes", how="left")
+    # print(final_world["2014 [YR2014]"])
     return final_world
 # print (load_data())
+
+# def main():
 
