6730b18a936746fde5d77a81e34f08427ae5d06a
khadra <67019030+khadra1@users.noreply.github.com>
2022-06-27 16:58:15 +0100

    add css

--- static/js/app.js
+++ static/js/app.js
@@ -8,4 +8,5 @@ let myMap = L.map("map", {
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
+  console.log(alldata.name)
   
