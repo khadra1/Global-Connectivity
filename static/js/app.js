17456d47e8537a363fc223d21ae8d7361040e6eb
khadra <67019030+khadra1@users.noreply.github.com>
2022-06-27 17:15:47 +0100

    add css

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,12 @@
-let myMap = L.map("map", {
-    center: [45.52, -122.67],
-    zoom: 13
-  });
+// let myMap = L.map("map", {
+//     center: [45.52, -122.67],
+//     zoom: 13
+//   });
   
-  // Adding a tile layer (the background map image) to our map:
-  // We use the addTo() method to add objects to our map.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  console.log(alldata.name)
+//   // Adding a tile layer (the background map image) to our map:
+//   // We use the addTo() method to add objects to our map.
+//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//   }).addTo(myMap);
+console.log(alldata.name);
   
