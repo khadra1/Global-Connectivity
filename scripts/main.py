35c46b0ab1242c484eab4fd7199a019f96d6473f
khadra <67019030+khadra1@users.noreply.github.com>
2022-07-02 00:29:48 +0100

    world map added

--- scripts/main.py
+++ scripts/main.py
@@ -9,7 +9,7 @@ def load_data(filter1, filter2):
     data_path = os.path.join(os.path.dirname(
         __file__), '..', 'data', 'world_bank_internet_data.csv')
     world_data = pd.read_csv(data_path, skipfooter=446, engine="python")
-    world_data.replace("..", 0)
+    world_data = world_data.replace("..", 0)
     # Rename the Country and Country Codes column to allow merging later on
     world_data.rename(
         columns={'Country Name': 'Country', 'Country Code': 'Codes'}, inplace=True)
@@ -35,59 +35,63 @@ def load_data(filter1, filter2):
     # Turn the Year columns into Rows
     data = pd.melt(final_world, id_vars=[
                    'Country', 'Codes', 'Series Name', 'Longitude', 'Latitude'], var_name='Year', value_name='Values')
+    data.rename(columns={'Series Name': 'SeriesName'},inplace=True)
+    data['Year']=data['Year'].astype(int)
+    data['Values']=data['Values'].astype(float)
+
     # Creating dataframes from each unique value in Series Name column
-    world1 = data[data['Series Name'] ==
-        'Individuals using the Internet (% of population)'].copy()
-    world2 = data[data['Series Name'] == 'Secure Internet servers'].copy()
-    world3 = data[data['Series Name'] ==
-        'Secure Internet servers (per 1 million people)'].copy()
-    world4 = data[data['Series Name'] ==
-        'Mobile cellular subscriptions'].copy()
-    world5 = data[data['Series Name'] ==
-        'Fixed broadband subscriptions'].copy()
-    world6 = data[data['Series Name'] ==
-        'Fixed broadband subscriptions (per 100 people)'].copy()
-    world7 = data[data['Series Name'] ==
-        'Fixed telephone subscriptions'].copy()
-    world8 = data[data['Series Name'] ==
-        'Fixed telephone subscriptions (per 100 people)'].copy()
+    # world1 = data[data['Series Name'] ==
+    #     'Individuals using the Internet (% of population)'].copy()
+    # world2 = data[data['Series Name'] == 'Secure Internet servers'].copy()
+    # world3 = data[data['Series Name'] ==
+    #     'Secure Internet servers (per 1 million people)'].copy()
+    # world4 = data[data['Series Name'] ==
+    #     'Mobile cellular subscriptions'].copy()
+    # world5 = data[data['Series Name'] ==
+    #     'Fixed broadband subscriptions'].copy()
+    # world6 = data[data['Series Name'] ==
+    #     'Fixed broadband subscriptions (per 100 people)'].copy()
+    # world7 = data[data['Series Name'] ==
+    #     'Fixed telephone subscriptions'].copy()
+    # world8 = data[data['Series Name'] ==
+    #     'Fixed telephone subscriptions (per 100 people)'].copy()
 
-    # Dropping Series Name column and renaming values column to the 8 unique values we extracted from Series Name column
-    world1.drop(columns=['Series Name'], inplace=True)
-    world1.rename(columns={
-                  'Values': 'Individuals using the Internet (% of population)'}, inplace=True)
-    world2.drop(columns=['Series Name'], inplace=True)
-    world2.rename(columns={'Values': 'Secure Internet servers'}, inplace=True)
-    world3.drop(columns=['Series Name'], inplace=True)
-    world3.rename(columns={
-                  'Values': 'Secure Internet servers (per 1 million people)'}, inplace=True)
-    world4.drop(columns=['Series Name'], inplace=True)
-    world4.rename(
-        columns={'Values': 'Mobile cellular subscriptions'}, inplace=True)
-    world5.drop(columns=['Series Name'], inplace=True)
-    world5.rename(
-        columns={'Values': 'Fixed broadband subscriptions'}, inplace=True)
-    world6.drop(columns=['Series Name'], inplace=True)
-    world6.rename(columns={
-                  'Values': 'Fixed broadband subscriptions (per 100 people)'}, inplace=True)
-    world7.drop(columns=['Series Name'], inplace=True)
-    world7.rename(
-        columns={'Values': 'Fixed telephone subscriptions'}, inplace=True)
-    world8.drop(columns=['Series Name'], inplace=True)
-    world8.rename(columns={
-                  'Values': 'Fixed telephone subscriptions (per 100 people)'}, inplace=True)
+    # # Dropping Series Name column and renaming values column to the 8 unique values we extracted from Series Name column
+    # world1.drop(columns=['Series Name'], inplace=True)
+    # world1.rename(columns={
+    #               'Values': 'Individuals using the Internet (% of population)'}, inplace=True)
+    # world2.drop(columns=['Series Name'], inplace=True)
+    # world2.rename(columns={'Values': 'Secure Internet servers'}, inplace=True)
+    # world3.drop(columns=['Series Name'], inplace=True)
+    # world3.rename(columns={
+    #               'Values': 'Secure Internet servers (per 1 million people)'}, inplace=True)
+    # world4.drop(columns=['Series Name'], inplace=True)
+    # world4.rename(
+    #     columns={'Values': 'Mobile cellular subscriptions'}, inplace=True)
+    # world5.drop(columns=['Series Name'], inplace=True)
+    # world5.rename(
+    #     columns={'Values': 'Fixed broadband subscriptions'}, inplace=True)
+    # world6.drop(columns=['Series Name'], inplace=True)
+    # world6.rename(columns={
+    #               'Values': 'Fixed broadband subscriptions (per 100 people)'}, inplace=True)
+    # world7.drop(columns=['Series Name'], inplace=True)
+    # world7.rename(
+    #     columns={'Values': 'Fixed telephone subscriptions'}, inplace=True)
+    # world8.drop(columns=['Series Name'], inplace=True)
+    # world8.rename(columns={
+    #               'Values': 'Fixed telephone subscriptions (per 100 people)'}, inplace=True)
 
-    # Resetting Index
-    world1.reset_index(drop=True, inplace=True)
-    world2.reset_index(drop=True, inplace=True)
-    world3.reset_index(drop=True, inplace=True)
-    world4.reset_index(drop=True, inplace=True)
-    world5.reset_index(drop=True, inplace=True)
-    world6.reset_index(drop=True, inplace=True)
-    world7.reset_index(drop=True, inplace=True)
-    world8.reset_index(drop=True, inplace=True)
-    world = pd.merge(pd.merge(pd.merge(pd.merge(pd.merge(pd.merge(
-        pd.merge(world1, world2), world3), world4), world5), world6), world7), world8)
+    # # Resetting Index
+    # world1.reset_index(drop=True, inplace=True)
+    # world2.reset_index(drop=True, inplace=True)
+    # world3.reset_index(drop=True, inplace=True)
+    # world4.reset_index(drop=True, inplace=True)
+    # world5.reset_index(drop=True, inplace=True)
+    # world6.reset_index(drop=True, inplace=True)
+    # world7.reset_index(drop=True, inplace=True)
+    # world8.reset_index(drop=True, inplace=True)
+    # world = pd.merge(pd.merge(pd.merge(pd.merge(pd.merge(pd.merge(
+    #     pd.merge(world1, world2), world3), world4), world5), world6), world7), world8)
     # Reading the ITU excel file region sheet to clean and turn into json object
     data_path = os.path.join(os.path.dirname(__file__), '..', 'data',
                              'ITU_regional_global_Key_ICT_indicator_aggregates_rev1_Jan_2022.xlsx')
@@ -123,12 +127,15 @@ def load_data(filter1, filter2):
     world_data = world_data.loc[world_data['Country'] == filter2]
     output['x'] = list(world_data.columns.values)[4:]
     output['y'] = world_data.values.tolist()[0]
-    output['Country'] = world['Country'].tolist()
-    output['Longitude'] = world['Longitude'].tolist()
-    output['Latitude'] = world['Latitude'].tolist()
-    output['Year'] = world['Year'].tolist()
-    output['Region'] = region_df['Individuals using the Internet'].tolist()
-    
+    # output['Country'] = world['Country'].tolist()
+    # output['Longitude'] = world['Longitude'].tolist()
+    # output['Latitude'] = world['Latitude'].tolist()
+    # output['Year'] = world['Year'].tolist()
+    # output['Region'] = region_df['Individuals using the Internet'].tolist()
+    # output['world1']=world1.to_json(orient='index', indent=4)
+    output['dataWorld']=data.to_json(orient='index', indent=4)
+
+
 
 
   
